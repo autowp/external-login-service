1148539e6117a3530ff80ea26063326519e901ce
Dmitry <dmitry@pereslegin.ru>
2017-07-20 02:16:49 +0300

    v2: google-plus

---
+++ test/GooglePlusTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AutowpTest\ExternalLoginService;
+
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\GoogleUser;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+use Autowp\ExternalLoginService\GooglePlus;
+use Autowp\ExternalLoginService\PluginManager;
+use Autowp\ExternalLoginService\Result;
+
+class GooglePlusTest extends AbstractHttpControllerTestCase
+{
+    protected $appConfigPath = __DIR__ . '/_files/config/application.config.php';
+
+    protected function setUp()
+    {
+        if (! $this->appConfigPath) {
+            throw new \Exception("Application config path not provided");
+        }
+
+        $this->setApplicationConfig(include $this->appConfigPath);
+
+        parent::setUp();
+    }
+
+    private function mockProvider()
+    {
+        $providerMock = $this->getMockBuilder(\League\OAuth2\Client\Provider\Google::class)
+            ->setConstructorArgs([[
+                'graphApiVersion' => 'v2.10'
+            ]])
+            ->getMock();
+
+        $providerMock->method('getResourceOwner')->willReturnCallback(function () {
+            return new GoogleUser([
+                'id'          => 'user_id',
+                'displayName' => 'UserName',
+                'url'         => 'http://example.com/user_id',
+                'image' => [
+                    'url' => 'http://example.com/user_id.jpg'
+                ],
+                'gender'      => 'male',
+                'language'    => 'en'
+            ]);
+        });
+
+        $service = $this->getService();
+
+        $service->setProvider($providerMock);
+    }
+
+    /**
+     * @return GooglePlus
+     */
+    private function getService()
+    {
+        $manager = $this->getApplicationServiceLocator()->get('ExternalLoginServiceManager');
+
+        $this->assertInstanceOf(PluginManager::class, $manager);
+
+        $service = $manager->get('google-plus');
+
+        $this->assertInstanceOf(GooglePlus::class, $service);
+
+        return $service;
+    }
+
+    public function testUrls()
+    {
+        $service = $this->getService();
+
+        $loginUrl = $service->getLoginUrl();
+
+        $this->assertRegExp(
+            '|https://accounts\.google\.com/o/oauth2/auth' .
+                '\?scope='.
+                    'https%3A%2F%2Fwww\.googleapis\.com%2Fauth%2Fplus\.me%20' .
+                    'https%3A%2F%2Fwww\.googleapis\.com%2Fauth%2Fuserinfo\.email%20'.
+                    'https%3A%2F%2Fwww\.googleapis\.com%2Fauth%2Fuserinfo\.profile' .
+                '&state=[a-z0-9]+&response_type=code&approval_prompt=auto' .
+                '&client_id=xxxx&authuser=-1|iu',
+            $loginUrl
+        );
+    }
+
+    public function testThrowsCredentialRequired()
+    {
+        $this->expectException(IdentityProviderException::class);
+
+        $service = $this->getService();
+
+        $service->setAccessToken('example_access_token');
+        $service->getData([]);
+    }
+
+    public function testGetData()
+    {
+        $this->mockProvider();
+
+        $service = $this->getService();
+
+        $service->setAccessToken('example_access_token');
+
+        $data = $service->getData([]);
+
+        $this->assertInstanceOf(Result::class, $data);
+        $this->assertEquals('UserName', $data->getName());
+        $this->assertEquals('user_id', $data->getExternalId());
+        $this->assertEquals('http://example.com/user_id', $data->getProfileUrl());
+        $this->assertEquals('http://example.com/user_id.jpg', $data->getPhotoUrl());
+    }
+}
--- test/_files/config/autoload/local.php
+++ test/_files/config/autoload/local.php
@@ -4,17 +4,22 @@ namespace Autowp\ExternalLoginService;
 
 return [
     'external_login_services' => [
-        Github::class => [
-            'clientId'     => 'xxxx',
-            'clientSecret' => 'yyyy',
-            'redirectUri'  => 'http://example.com/'
-        ],
         Facebook::class => [
             'clientId'        => 'xxxx',
             'clientSecret'    => 'yyyy',
             'redirectUri'     => 'http://example.com/',
             'scope'           => ['public_profile', 'user_friends'],
             'graphApiVersion' => 'v2.10'
+        ],
+        Github::class => [
+            'clientId'     => 'xxxx',
+            'clientSecret' => 'yyyy',
+            'redirectUri'  => 'http://example.com/'
+        ],
+        GooglePlus::class => [
+            'clientId'        => 'xxxx',
+            'clientSecret'    => 'yyyy',
+            'redirectUri'     => 'http://example.com/',
         ]
     ]
 ];
